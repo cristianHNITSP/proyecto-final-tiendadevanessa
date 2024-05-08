5eeccc0838c3bfe9392a4297597ee0af78d83f97
Fractrhythm <cristian.hernandez.240602@gmail.com>
2024-05-08 02:30:40 -0600

    cambios significativos

--- marketMAIN/products.py
+++ marketMAIN/products.py
@@ -2,19 +2,11 @@ from flask import Flask, Blueprint, render_template, request, redirect, url_for
 
 bp = Blueprint('products', __name__, url_prefix='/products')
 
-@bp.route('/viewproduct')
-def viewproduct():
-    return render_template('products/view-product.html')
+@bp.route('/warehouse')
+def warehouse():
+    return render_template('products/warehouse.html')
 
-@bp.route('/editproduct')
-def editproduct():
-    return render_template('products/edit-product.html')
 
-@bp.route('/addproduct')
-def addproduct():
-    return render_template('products/add-product.html')
-
-@bp.route('/QR')
-def scanner():
-    return render_template('products/tester.html')
-
+@bp.route('/Manage_warehouse')
+def managewarehouse():
+    return render_template('products/Manage-warehouse.html')
